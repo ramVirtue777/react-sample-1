3a746f67648de9c3ff6233d504ff29369d8ef2ee
ram_virtue <ramprashanth@siamcomputing.com>
2019-05-23 12:48:11 +0530

    Updates

---
+++ precache-manifest.1409286cdebe32b2f3de24f016371972.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b715ae13f950ad2123bd95c2af3513",
+    "url": "/react-sample-1/index.html"
+  },
+  {
+    "revision": "ffe2d9126c9935ea9af8",
+    "url": "/react-sample-1/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-sample-1/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ffe2d9126c9935ea9af8",
+    "url": "/react-sample-1/static/js/main.9f4a294c.chunk.js"
+  },
+  {
+    "revision": "9d3175c58363e078ddd6",
+    "url": "/react-sample-1/static/js/runtime~main.4be59fd7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-sample-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
